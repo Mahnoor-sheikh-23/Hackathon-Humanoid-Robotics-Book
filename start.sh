1b6e3dff826ee0b9dd58834cd9d9672be51bf2b5
mahnoor adnan <mahnooradnan479@gmail.com>
2025-12-18 16:51:52 +0500

    make some change

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-set -e   # exit on any error
+set -e
 
 # Move into backend folder
 cd backend
 
 # Install dependencies
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
 
 # Ensure PYTHONPATH includes current directory
 export PYTHONPATH=.
 
-# Start FastAPI with the Railway injected port
-python3 -m uvicorn main:app --host 0.0.0.0 --port $PORT
+# Start FastAPI with Railway injected port
+uvicorn main:app --host 0.0.0.0 --port $PORT
