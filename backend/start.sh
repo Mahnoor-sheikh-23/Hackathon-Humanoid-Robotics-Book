1817d17e10d7c5e14528a9ac4b42203615ed11e0
mahnoor adnan <mahnooradnan479@gmail.com>
2025-12-18 15:35:23 +0500

    add some changes in start.sh file

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Start the FastAPI application with the PORT environment variable
+#!/bin/bash
+cd backend  # go into backend folder
 pip install -r requirements.txt
 export PYTHONPATH=.
 uvicorn main:app --host 0.0.0.0 --port $PORT
+
