b628711ff93c54b37560fbbc61c22e218aee9be4
mahnoor adnan <mahnooradnan479@gmail.com>
2025-12-18 16:29:24 +0500

    remove cd

--- backend/start.sh
+++ backend/start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Start the FastAPI application with the PORT environment variable
 #!/bin/bash
-cd backend  # go into backend folder
 pip install -r requirements.txt
 export PYTHONPATH=.
 uvicorn main:app --host 0.0.0.0 --port $PORT
