e9f1c50614fc2fef41b554f19639a9d2cc4b0df0
mahnoor adnan <mahnooradnan479@gmail.com>
2025-12-18 16:54:08 +0500

    in root

--- start.sh
+++ start.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 set -e
-
-# Move into backend folder
 cd backend
-
-# Install dependencies
-pip install --upgrade pip
-pip install -r requirements.txt
-
-# Ensure PYTHONPATH includes current directory
 export PYTHONPATH=.
-
-# Start FastAPI with Railway injected port
 uvicorn main:app --host 0.0.0.0 --port $PORT
