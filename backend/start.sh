41e4a9c327f70d137557625217a80cc8f866716f
mahnoor adnan <mahnooradnan479@gmail.com>
2025-12-18 16:33:58 +0500

    delete start

--- backend/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Start the FastAPI application with the PORT environment variable
-#!/bin/bash
-pip install -r requirements.txt
-export PYTHONPATH=.
-uvicorn main:app --host 0.0.0.0 --port $PORT
