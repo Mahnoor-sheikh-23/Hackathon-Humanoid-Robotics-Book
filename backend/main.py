3071e612dd1fd34678d31e6e34ef2f018f3478d1
mahnoor adnan <mahnooradnan479@gmail.com>
2025-12-18 18:57:03 +0500

    changes in main.py

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,10 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # In production, replace with specific origins
+      allow_origins=[
+        "http://localhost:3000",  # for local dev
+        "https://mahnoor-sheikh-23.github.io"
+    ],  # In production, replace with specific origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
