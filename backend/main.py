0cbfe570ed154429032590035db37daaf2bc55df
mahnoor adnan <mahnooradnan479@gmail.com>
2025-12-19 16:38:40 +0500

    remove *

--- backend/main.py
+++ backend/main.py
@@ -26,9 +26,7 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-      allow_origins=[
-        "http://localhost:3000",  # for local dev
-        "https://mahnoor-sheikh-23.github.io/Hackathon-Humanoid-Robotics-Book/"  # GitHub Pages with project path
+      allow_origins=["*" # GitHub Pages with project path
     ],  # In production, replace with specific origins
     allow_credentials=True,
     allow_methods=["*"],
